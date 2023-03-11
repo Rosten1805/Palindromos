810c6a64b52c804b6a5fad0f4c250f528927a427
Cristina Cañadas Gómez - Limón <rosten1805@protonmail.com>
2023-03-11 20:41:25 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa una cadena de caracteres: ");
+        string cadena = Console.ReadLine();
+
+        cadena = cadena.ToLower().Replace(" ", "");
+
+        char[] arrayInverso = cadena.ToCharArray();
+        Array.Reverse(arrayInverso);
+
+        string cadenaInversa = new string(arrayInverso);
+        
+        if (cadena.Equals(cadenaInversa))
+        {
+            Console.WriteLine("La cadena es un palíndromo.");
+        }
+        else
+        {
+            Console.WriteLine("La cadena no es un palíndromo.");
+        }
+    }
+}
